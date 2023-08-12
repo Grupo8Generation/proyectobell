027403322157932df28ffe072a7963db46de5b7a
Crazygufer <grupo8.cohorte6.gen@gmail.com>
2023-08-12 00:10:09 -0400

    Se centro el nav, se agrego el boton para el nav-collapsed, se hizo el footer responsive.

--- client/src/componentes/NavBar.jsx
+++ client/src/componentes/NavBar.jsx
@@ -1,7 +1,5 @@
 import './NavBars.css'
 import { Link, NavLink } from 'react-router-dom';
-import Badge from '@mui/material/Badge'; // Importa el componente Badge de Material-UI
-import ShoppingCart from '@mui/icons-material/ShoppingCart';
 import CarritoButton from '../pages/Carrito/CarritoButton';
 
 
@@ -13,39 +11,40 @@ const NavBar = () => {
             <nav id="menu" className="navbar navbar-expand-lg align-items-center justify-content-center ">
 
                 <img src='src/img/Frame_15-removebg-preview.png' alt="LogoBell"></img>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button> </div>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-5 ">
-                        <li className="nav-item">
-                            <NavLink className="nav-link NosLink" to='/home' >Nosotros</NavLink>
-                        </li>
-                        <li className="nav-item text-pink">
-                            <a className="nav-link " href="#Hashtagss">Pedido personalizado</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link " href="#">Envíos</a>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to='/pasarela' className="nav-link " >Preguntas frecuentes</NavLink>
-                        </li>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-5 ">
+                            <li className="nav-item">
+                                <NavLink className="nav-link NosLink" to='/home' >Nosotros</NavLink>
+                            </li>
+                            <li className="nav-item text-pink">
+                                <a className="nav-link " href="#Hashtagss">Pedido personalizado</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link " href="#">Envíos</a>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to='/pasarela' className="nav-link " >Preguntas frecuentes</NavLink>
+                            </li>
 
 
-                        <li className="nav-item">
-                            <NavLink to='/tienda' className="nav-link" >Tienda</NavLink>
-                        </li>
-                     <li className='nav-item'> 
-                     <CarritoButton/>
-                    </li>  
-                    </ul>
+                            <li className="nav-item">
+                                <NavLink to='/tienda' className="nav-link" >Tienda</NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <CarritoButton />
+                            </li>
+                        </ul>
 
 
 
-                </div>
+                    </div>
 
-
+</div>
             </nav>
 
 
