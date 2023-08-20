b391aceb137af44152dfe4ad444752322412bdbf
Crazygufer <grupo8.cohorte6.gen@gmail.com>
2023-08-19 23:47:43 -0400

    Se aplicaron estilos a las cards de carrito Card.jsx

--- client/src/pages/Carrito/Card.jsx
+++ client/src/pages/Carrito/Card.jsx
@@ -21,7 +21,7 @@ const Card1 = ({ imagenUrl, nombre, precio, handleAgregar, handleQuitar, hanldeA
         <div className="card">
             <img className="card-img-top" src={imagenUrl} alt={nombre} />
             <div className="card-contenido">
-                <p className=' hashtag'>#Amigurumi</p>
+                <p className='card-hashtag'>#Amigurumi</p>
                 <h2 className='card-nombre'>{nombre}</h2>
                 <h3 className='card-precio'>{precio}</h3>
             </div>
